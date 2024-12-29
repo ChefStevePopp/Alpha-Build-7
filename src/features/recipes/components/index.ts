3a0c79b96079502ebf66cac35b4b357abecd0062
Chef Steve Popp <office@memphisfirebbq.com>
2024-12-28 19:45:53 -0500

    Update src/features/recipes/components/index.ts [BoltSync]

--- src/features/recipes/components/index.ts
+++ src/features/recipes/components/index.ts
@@ -1,4 +1,4 @@
 export { RecipeManager } from './RecipeManager';
-export { RecipeCard } from './RecipeCard';
+export { default as RecipeCard } from './RecipeCard';
 export { RecipeEditorModal } from './RecipeEditor';
 export { RecipeImportModal } from './RecipeImportModal';
