78509d98fbdb8d0c3db9ca23ab4ea81358fbe27f
Chef Steve Popp <office@memphisfirebbq.com>
2024-12-28 19:45:49 -0500

    Update src/features/recipes/types/storage.ts [BoltSync]

--- src/features/recipes/types/storage.ts
+++ src/features/recipes/types/storage.ts
@@ -1,30 +1,20 @@
-import type { Recipe } from './recipe';
-
-export type Temperature = {
+export interface Temperature {
   value: number;
   unit: 'F' | 'C';
   tolerance: number;
-};
+}
 
-export type ShelfLife = {
+export interface ShelfLife {
   value: number;
   unit: 'hours' | 'days' | 'weeks';
-};
+}
 
-export type Storage = Required<NonNullable<Recipe['storage']>>;
-
-export const DEFAULT_STORAGE: Required<NonNullable<Recipe['storage']>> = {
-  location: '',
-  container: '',
-  containerType: '',
-  temperature: {
-    value: 40,
-    unit: 'F',
-    tolerance: 2
-  },
-  shelfLife: {
-    value: 1,
-    unit: 'days'
-  },
-  specialInstructions: []
-};
+export interface Storage {
+  location?: string;
+  container?: string;
+  containerType?: string;
+  labelImageUrl?: string;
+  temperature?: Temperature;
+  shelfLife?: ShelfLife;
+  specialInstructions?: string[];
+}
